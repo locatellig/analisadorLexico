59d993c3f36fdc4de21f008b18c9f70d9a9f68d1
locatellig <guilhermeloka12@gmail.com>
2024-11-15 18:33:20 -0300

    Analisador léxico parte - 03 - Implementada matriz dinamica, validação de input e melhorias no código

--- js/app.js
+++ js/app.js
@@ -2,51 +2,53 @@ class AnalisadorLexico {
     #matriz
     #alfabeto
     constructor() {
-        this.#matriz = Array.from({ length: 100 }, () => Array(27).fill(null))
-
+        this.#matriz = Array()
         //Adiciona alfabeto convertendo ASCII para letra correspondente
         this.#alfabeto = [];
         for (let i = 65; i <= 90; i++) {
             this.#alfabeto.push(String.fromCharCode(i));
         }
 
-        this.#alimentaMatriz(bd.recuperarTokens())
+        this.alimentaMatriz(bd.recuperarTokens())
     }
 
     getMatriz() {
         return this.#matriz
     }
 
-    adicionaToken() {
-        let tokenElement = document.getElementById('token')
-
-        this.#alimentaMatriz(tokenElement)
-
-        tabela.montaTabela()
-
-        let token = new Token(tokenElement.value)
-
-        bd.gravarToken(token)
-
-        tabela.montaTabelaTokens()
+    getAlfabeto() {
+        return this.#alfabeto
     }
 
-    #alimentaMatriz(token) {
-
+    alimentaMatriz(token) {
+        let i = 0
         if (Array.isArray(token)) {
             token.forEach((value) => {
-                for(let i = 0; i <= value.token.length - 1; i++) {
+                for (i = 0; i <= value.token.length - 1; i++) {
                     let posicaoAlfabeto = this.#alfabeto.indexOf(value.token[i].toUpperCase()) + 1
+
+                    this.#adicionaArrayDinamico(i)
+
                     this.#matriz[i][posicaoAlfabeto] = i
                 }
+                this.#matriz[i - 1][0] = 'F'
             })
         } else {
-            for (let i = 0; i <= token.value.length - 1; i++) {
+            for (i = 0; i <= token.value.length - 1; i++) {
                 let posicaoAlfabeto = this.#alfabeto.indexOf(token.value[i].toUpperCase()) + 1
+
+                this.#adicionaArrayDinamico(i)
+
                 this.#matriz[i][posicaoAlfabeto] = i
             }
+            this.#matriz[i - 1][0] = 'F'
         }
+    }
 
+    #adicionaArrayDinamico(posicaoArray) {
+        if (this.#matriz[posicaoArray] === undefined) {
+            this.#matriz.push(Array(27).fill(null))
+        }
     }
 }
 
@@ -77,7 +79,6 @@ class Tabela {
         let matriz = analisadorLexico.getMatriz()
         let bodyTabela = document.getElementById('matrizBody')
         bodyTabela.innerHTML = ''
-        console.log(matriz)
         for (let i = 0; i < matriz.length; i++) {
 
             let linhaTabela = bodyTabela.insertRow()
@@ -85,9 +86,10 @@ class Tabela {
             for (let x = 0; x < matriz[i].length; x++) {
                 let celulaTabela = linhaTabela.insertCell()
                 if (x == 0) {
-                    celulaTabela.innerHTML = i + 1
+                    celulaTabela.innerHTML = (matriz[i][x] == 'F') ? ((i + 1) + '*') : i + 1
                 } else {
                     celulaTabela.innerHTML = matriz[i][x]
+                    celulaTabela.id = `${i}_${x}`
                 }
             }
         }
@@ -150,3 +152,50 @@ class BD {
 let bd = new BD()
 let analisadorLexico = new AnalisadorLexico()
 let tabela = new Tabela()
+
+function adicionaTokenBD() {
+    let tokenElement = document.getElementById('token')
+    analisadorLexico.alimentaMatriz(tokenElement)
+
+    tabela.montaTabela()
+
+    let token = new Token(tokenElement.value)
+
+    bd.gravarToken(token)
+
+    tabela.montaTabelaTokens()
+}
+
+let tamanhoAnterior = 0
+let ultimoCaractere = ''
+
+function verificaPosicao(texto) {
+    let matriz = analisadorLexico.getMatriz()
+    let alfabeto = analisadorLexico.getAlfabeto()
+    const tamanhoAtual = texto.length
+
+    if ((tamanhoAtual == 0) || (matriz[tamanhoAtual-1] != undefined)) {
+        //apagou o caractere
+        if (tamanhoAtual < tamanhoAnterior) {
+            let posicaoAlfabeto = alfabeto.indexOf(ultimoCaractere.toUpperCase()) + 1
+            let tdLetra = document.getElementById(`${tamanhoAtual}_${posicaoAlfabeto}`)
+            if (tdLetra != null) {
+                tdLetra.className = ''
+            }
+        }
+        else {
+            let posicaoAlfabeto = alfabeto.indexOf(texto.slice(-1).toUpperCase()) + 1
+            let tdLetra = document.getElementById(`${tamanhoAtual - 1}_${posicaoAlfabeto}`)
+            if (posicaoAlfabeto > 0) {
+                if (matriz[tamanhoAtual - 1][posicaoAlfabeto] === (tamanhoAtual - 1)) {
+                    tdLetra.className = 'selecionado_valido'
+                } else {
+                    tdLetra.className = 'selecionado_invalido'
+                }
+            }
+        }
+    }
+
+    tamanhoAnterior = texto. length
+    ultimoCaractere = texto.slice(-1)
+}
