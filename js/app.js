84a11d3a8d4b18eb67a9c6539dec59967cf172cf
locatellig <guilhermeloka12@gmail.com>
2024-11-20 15:20:33 -0300

    Analisador léxico parte - 07 - finalização do front-end

--- js/app.js
+++ js/app.js
@@ -284,7 +284,7 @@ function adicionaTokenBD() {
     bd.gravarToken(token)
 
     tabela.montaTabelaTokens()
-    tabela.montaPaginacao()
+    paginacao.montaPaginacao()
 
     tokenElement.value = ''
 }
